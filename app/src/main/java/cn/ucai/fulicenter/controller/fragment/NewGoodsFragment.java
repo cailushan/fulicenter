5bc5b1305bbb34432fccd2972e912f0974ac79e1
cls <731058610@qq.com>
2017-01-11 13:59:47 +0800

    新品Fragment下载数据并显示

---
+++ app/src/main/java/cn/ucai/fulicenter/controller/fragment/NewGoodsFragment.java
@@ -0,0 +1,93 @@
+package cn.ucai.fulicenter.controller.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.controller.adapter.GoodsAdapter;
+import cn.ucai.fulicenter.model.bean.NewGoodsBean;
+import cn.ucai.fulicenter.model.net.IModelNewGoods;
+import cn.ucai.fulicenter.model.net.ModelNewGoods;
+import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.utils.ConvertUtils;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NewGoodsFragment extends Fragment {
+
+    GridLayoutManager gm;
+    @BindView(R.id.rv)
+    RecyclerView rv;
+    @BindView(R.id.srl)
+    SwipeRefreshLayout srl;
+    @BindView(R.id.tvRefresh)
+    TextView tvRefresh;
+
+    GoodsAdapter mAdapter;
+    ArrayList<NewGoodsBean> mList = new ArrayList<>();
+    IModelNewGoods mModel;
+    int pageId = 1;
+
+    public NewGoodsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View layout = inflater.inflate(R.layout.fragment_new_goods, container, false);
+        ButterKnife.bind(this, layout);
+
+        initView();
+        mModel = new ModelNewGoods();
+        initData();
+        return layout;
+    }
+
+    private void initData() {
+        mModel.downData(getContext(), I.CAT_ID, pageId, new OnCompleteListener<NewGoodsBean[]>() {
+            @Override
+            public void onSuccess(NewGoodsBean[] result) {
+                if (result != null && result.length > 0) {
+                    ArrayList<NewGoodsBean> list = ConvertUtils.array2List(result);
+                    mAdapter.initData(list);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+    }
+
+    private void initView() {
+        srl.setColorSchemeColors(
+                getResources().getColor(R.color.google_blue),
+                getResources().getColor(R.color.google_green),
+                getResources().getColor(R.color.google_yellow),
+                getResources().getColor(R.color.google_red));
+        gm = new GridLayoutManager(getContext(), I.COLUM_NUM);
+        rv.setLayoutManager(gm);
+        rv.setHasFixedSize(true);
+        mAdapter = new GoodsAdapter(getContext(), mList);
+        rv.setAdapter(mAdapter);
+
+    }
+}
